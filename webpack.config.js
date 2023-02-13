411403f43abbfca6a7a92d66afab285ee9bf2aec
Kaiyuan Ma <frankmkmk1@gmail.com>
2023-02-13 15:17:09 -0500

    edited webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   devtool: "source-map",
   entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
