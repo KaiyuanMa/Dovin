2c7725b5eceb9907e4e3a23da65be53aec47e2a2
Kaiyuan Ma <frankmkmk1@gmail.com>
2023-02-13 15:22:16 -0500

    fixed webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,14 @@
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.config");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const prodConfig = {
   mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
 
 module.exports = merge(commonConfig, prodConfig);
