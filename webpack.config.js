39ccf0d9c7b60d53f196cd8a9d20c88f98af3e18
Kaiyuan Ma <frankmkmk1@gmail.com>
2023-02-20 12:34:10 -0500

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require("path");
 
 module.exports = {
   devtool: "source-map",
-  // entry: "./src/index.js",
-  // output: {
-  //   filename: "[name].bundle.js",
-  //   path: path.resolve(__dirname, "dist"),
-  // },
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   module: {
     rules: [
       {
